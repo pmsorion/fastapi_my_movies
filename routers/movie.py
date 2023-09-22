68c389b71fb8a11cee144c426a8f656a84d4155c
pmsorion <garciafran@hotmail.com>
2023-09-22 13:32:38 -0500

    services for get movies

--- routers/movie.py
+++ routers/movie.py
@@ -11,6 +11,8 @@ from config.database import Session
 from models.movie import Movie as MovieModel
 from middlewares.jwt_bearer import JWTBearer
 
+from services.movie import MovieService
+
 import datetime
 
 movie_router = APIRouter()
@@ -45,7 +47,7 @@ class Movie(BaseModel):
 @movie_router.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
     db = Session()
-    result = db.query(MovieModel).all()
+    result = MovieService(db).get_movies()
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 
